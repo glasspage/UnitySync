78a0b0e61f0997c20dabb1393dc47e971611b868
glasspage <98078440+glasspage@users.noreply.github.com>
2026-09-14 16:42:59 -0700

    Show UnitySync version 0.7.0

--- Editor/UnitySyncWindow.cs
+++ Editor/UnitySyncWindow.cs
@@ -18,7 +18,7 @@ namespace Glasspage.UnitySync
         private const string HostAddressPreference = "Glasspage.UnitySync.HostAddress";
         private const string PortPreference = "Glasspage.UnitySync.Port";
         // Keep this in sync with package.json when releasing a new UnitySync version.
-        private const string Version = "0.6.0";
+        private const string Version = "0.7.0";
         private const string HeaderTitle = "UnitySync v" + Version;
         private const int DefaultPort = 47832;
 
